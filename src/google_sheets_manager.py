939daebd425ab50ae4a7819e65b31c9c5bce9ac0
WeronikaMagdalena <weronika.wooojcik@gmail.com>
2025-02-06 13:18:46 +0100

    Headers validation

--- src/google_sheets_manager.py
+++ src/google_sheets_manager.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Google Sheets API Setup
 SCOPES = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 CREDENTIALS_FILE = "../service_account.json"
-SPREADSHEET_ID = "1rjI5FQcNPfaWpbOD7bmndpMwJooOshzCQQomBMYlQKg"
+SPREADSHEET_ID = "1rjI5FQcNPfaWpbOD7bmndpMwJooOshzCQQomBMYlQKg"  #to ENV VAR or other solution
 
 
 def upload_to_google_sheets(file_path):
@@ -24,9 +24,18 @@ def upload_to_google_sheets(file_path):
         df.reset_index(drop=True, inplace=True)
         df = df.fillna("")
 
+        """Headers validation"""
+        existing_headers = sheet.row_values(1)
+        if existing_headers != df.columns.tolist():
+            print("⚠️ Error: Headers do not match. Data upload aborted.")
+            print(f"Google Sheets Headers: {existing_headers}")
+            print(f"File Headers: {df.columns.tolist()}")
+            return False
+
         """Data upload"""
-        sheet.clear()  # remove
-        sheet.append_rows([df.columns.tolist()] + df.values.tolist())
+        # sheet.clear()
+        sheet.append_rows(df.values.tolist())
+        # sheet.append_rows([df.columns.tolist()] + df.values.tolist())
 
         return True
 
